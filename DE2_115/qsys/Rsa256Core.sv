165d15f8115f433a30a185adb134841250c51903
TonyHsu <sunprince12014@gmail.com>
2015-10-22 23:34:08 +0800

    Partially finished - Need adding Multiply Block FSM

--- DE2_115/qsys/Rsa256Core.sv
+++ DE2_115/qsys/Rsa256Core.sv
@@ -10,46 +10,154 @@ module Rsa256Core(
 );
 // a ^ e mod n
 
-logic [255:0] ans_r, ans_w, pow_r, pow_w;
-logic [7:0] now_r, now_w;
-logic started_r, started_w;
+//Main Block State
+localparam S_MAIN_IDLE  = 0;
+localparam S_MAIN_BEGIN = 1;//to initialize core_state
+localparam S_MAIN_CALC  = 2;
+localparam S_MAIN_END = 3;
 
-assign o_finished = !started_r;
-assign o_a_pow_e = ans_r;
+//Power Block State
+localparam S_POW_IDLE        = 0;
+localparam S_POW_START       = 1;
+localparam S_POW_PREPROCESS  = 2;
+localparam S_POW_LOOP_READY  = 3;
+localparam S_POW_WAIT_NEEDIT = 4;
+localparam S_POW_LOOP        = 5;
+localparam S_POW_WAIT        = 6;
+localparam S_POW_END         = 7;
 
-always_comb begin
-	started_w = started_r;
-	ans_w = ans_r;
-	now_w = now_r;
-	pow_w = pow_r;
-	if (i_start && !started_r) begin
-		started_w = 1;
-		ans_w = 0;
-		pow_w = i_a;
-		now_w = 0;
-	end else if (started_r) begin
-		if (i_e & (1 << now_r))
-			ans_w = (ans_r * pow_r) % i_n;
-		pow_w = (pow_r * pow_r) % i_n;
-		if (now_r == 0)
-			started_w = 0;
-		else
-			now_w = now_r + 1;
+//Mutiply Block State
+localparam S_MUL_IDLE  = 0;
+localparam S_MUL_START = 1;
+localparam S_MUL_CALC  = 2;
+localparam S_MUL_END   = 3;
+
+
+logic[1:0] main_state_r,main_state_w;
+logic[2:0] pow_state_r,pow_state_w;
+logic[1:0] mul_state_r,mul_state_w;
+logic[15:0] pow_cnt_r,pow_cnt_w,mul_cnt_r,mul_count_w,pre_cnt_r,pre_cnt_w;
+logic[255:0] a256_r,a256_w;
+logic[255:0] ans_r,ans_w;
+logic[255:0] mul_ans_r,mul_ans_w;
+
+//task InitCalc;
+//endtask
+
+//task Preprocess;
+	//input [255] beforeshift;
+	//if(pre_cnt_r == 0)begin
+		//pow_state_w = S_POW_LOOP;
+		//pre_cnt_w = 255;
+		//return beforeshift;
+	//end
+	//else begin
+		//pre_cnt_w = pre_cnt_r - 1;
+	//end
+//endtask
+
+always_comb begin Main
+
+	//Main Block
+	case(main_state_r)
+		S_MAIN_IDLE:begin
+			if(i_start)	main_state_w = S_MAIN_START;
+			else	main_state_w = S_MAIN_IDLE;
+		end
+
+		S_MAIN_START:begin
+			pow_state_w = S_POW_START;
+			main_state_w = S_MAIN_CALC;
+		end
+
+		S_MAIN_CALC:begin
+			if(pow_state_r == S_POW_END)	main_state_w = S_MAIN_IDLE;
+		end
+	
+	//Power-Mont Block
+	case(pow_state_r)
+		S_POW_START:begin
+			pow_state_w = S_POW_PREPROCESS;
+			pre_cnt_w = 255;
+			a256_w = a;
+		end
+
+		S_POW_PREPROCESS:
+			if(a256_r >= n)	a256_w = a256_r - n;
+			else	a256_w = a256_r;
+			if(pre_cnt_r == 0)begin
+				pow_state_w = S_POW_LOOP_READY;
+				pre_cnt_w = 255;
+				pow_cnt_w = 255;
+			else
+				pre_cnt_w = pre_cnt_r - 1;
+			end
+		end
+
+		S_POW_LOOP_READY:
+			ans_w = 1;
+			if (e & (1 << pow_cnt_r)) begin //need_it
+				pow_state_w = S_POW_WAIT_NEEDIT;
+				mul_state_w = S_MUL_START;
+			end
+			else begin
+				pow_state_w = S_POW_LOOP;
+				mul_state_w = S_MUL_IDLE;
+			end
+		end
+
+		S_POW_WAIT_NEEDIT:
+			if(mul_state_r == S_MUL_END)begin
+				mul_state_r = S_MUL_IDLE;
+				pow_state_r = S_POW_LOOP;
+				ans_w = mul_ans_r;
+			end
+		end
+
+		S_POW_LOOP:
+			
+
+		end
+
+		S_POW_WAIT:
+
+		end
+
+		//S_MAIN_END:begin //reset
+			
+		//end
+end Main
+
+always_ff @(posedge i_clk or posedge i_rst) begin
+	if (i_rst) begin
+		main_state_r <= S_MAIN_IDLE;
+		pow_state_r <= S_POW_IDLE;
+		mul_state_r <= S_MUL_IDLE;
+
+		pre_cnt_r <= 0;
+		pow_cnt_r <= 0;
+		mul_cnt_r <= 0;
+
+		ans_r <= 0;
+		mul_ans_r <= 0;
+
+		a256_r <=0
+	end
+	else begin
+		if (i_start) begin
+		  a <= i_a;
+		  e <= i_e;
+		  n <= i_n;
+		end
+		main_state_r <= main_state_w;
+		pow_state_r <= pow_state_w;
+		mul_state_r <= mul_state_w;
+		pre_cnt_r <= pre_cnt_w;
+		pow_cnt_r <= pow_cnt_w;
+		mul_cnt_r <= mul_cnt_w;
+		ans_r <= ans_w;
+		mul_ans_r <= mul_ans_w;
+		a256_r <= a256_w;
 	end
 end
-
-always_ff @(posedge i_clk or posedge i_rst) begin : statement_label
-	if (i_rst) begin
-		started_r <= 0;
-		ans_r <= 0;
-		pow_r <= 0;
-		now_r <= 0;
-	end else begin
-		started_r <= started_w;
-		ans_r <= ans_w;
-		pow_r <= pow_w;
-		now_r <= now_w;
-	end
-	
-end : statement_label
 endmodule
